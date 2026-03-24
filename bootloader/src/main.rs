0c5633cc8ca8f36773d3533cb54cc14841056221
Mark Hatchard <hatchardm@btinternet.com>
2026-03-24 11:15:59 +0000

    Creating new bootloader

--- bootloader/src/main.rs
+++ bootloader/src/main.rs
@@ -4,6 +4,12 @@
 use uefi::prelude::*;
 
 #[entry]
-fn efi_main(_handle: Handle, _st: SystemTable<Boot>) -> Status {
+fn efi_main(_handle: Handle, st: SystemTable<Boot>) -> Status {
+    // SAFETY: UEFI guarantees the console is valid at boot.
+    let stdout = st.stdout();
+
+    // Print a simple message
+    let _ = stdout.output_string("Bulldog UEFI bootloader starting...\n");
+
     Status::SUCCESS
 }
