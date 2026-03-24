b4709bd16443fa6db525fd6abcb25e328b638240
Mark Hatchard <hatchardm@btinternet.com>
2026-03-24 14:30:53 +0000

    Creating new bootloader

--- bootloader/src/main.rs
+++ bootloader/src/main.rs
@@ -2,14 +2,21 @@
 #![no_main]
 
 use uefi::prelude::*;
+use uefi::CString16;
+use core::panic::PanicInfo;
 
 #[entry]
 fn efi_main(_handle: Handle, st: SystemTable<Boot>) -> Status {
-    // SAFETY: UEFI guarantees the console is valid at boot.
     let stdout = st.stdout();
 
-    // Print a simple message
-    let _ = stdout.output_string("Bulldog UEFI bootloader starting...\n");
+    let msg = CString16::try_from("Bulldog UEFI bootloader starting...\n").unwrap();
+    let _ = stdout.output_string(&msg);
 
     Status::SUCCESS
 }
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
