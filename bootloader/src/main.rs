6f96f3c246937a7885e2ecd45ac32122f99f22a8
Mark Hatchard <hatchardm@btinternet.com>
2026-03-24 15:37:43 +0000

    Creating new bootloader

--- bootloader/src/main.rs
+++ bootloader/src/main.rs
@@ -2,19 +2,22 @@
 #![no_main]
 
 use uefi::prelude::*;
-use uefi::CString16;
+use uefi::data_types::CStr16;
 use core::panic::PanicInfo;
 
 #[entry]
 fn efi_main(_handle: Handle, mut st: SystemTable<Boot>) -> Status {
     let stdout = st.stdout();
 
-    let msg = CString16::try_from("Bulldog UEFI bootloader starting...\n").unwrap();
-    let _ = stdout.output_string(&msg);
+    let mut buf = [0u16; 64];
+    let msg = CStr16::from_str_with_buf("Bulldog UEFI bootloader starting...\n", &mut buf)
+        .unwrap();
 
+    let _ = stdout.output_string(msg);
 
     Status::SUCCESS
 }
+
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
