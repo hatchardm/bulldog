6609e843c03d8f22fabef6c899f873ec1f7254bd
Mark Hatchard <hatchardm@btinternet.com>
2026-03-25 11:56:15 +0000

    Creating new bootloader

--- bootloader/src/main.rs
+++ bootloader/src/main.rs
@@ -1,9 +1,11 @@
 #![no_std]
 #![no_main]
 
+use core::panic::PanicInfo;
 use uefi::prelude::*;
 use uefi::data_types::CStr16;
-use core::panic::PanicInfo;
+
+use boot_proto::{BootInfo, Framebuffer, PixelFormat, MemoryRegion, MemoryRegionKind};
 
 #[entry]
 fn efi_main(_handle: Handle, mut st: SystemTable<Boot>) -> Status {
@@ -12,15 +14,26 @@ fn efi_main(_handle: Handle, mut st: SystemTable<Boot>) -> Status {
     let mut buf = [0u16; 64];
     let msg = CStr16::from_str_with_buf("Bulldog UEFI bootloader starting...\n", &mut buf)
         .unwrap();
-
     let _ = stdout.output_string(msg);
+
+    // Step 1: just prove we can *type-check* against BootInfo.
+    // We’ll fill this in with real GOP + memory map data next.
+    let _ = build_boot_info_stub();
 
     Status::SUCCESS
 }
 
+fn build_boot_info_stub() -> BootInfo {
+    BootInfo {
+        framebuffer: None,
+        physical_memory_offset: 0,
+        memory_regions: &[],
+    }
+}
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
+
 
